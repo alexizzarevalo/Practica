5381e2f5e16600dcd649888f2b2c42a85a2b3e45
Darwin Arevalo <dalexis.da@gmail.com>
2021-06-29 21:00:58 -0600

    Create 201709054.java
    Darwin Arevalo

---
+++ 201709054.java
@@ -0,0 +1,17 @@
+public class Practica {
+    public void ejercicio1() {
+        // Code
+    }
+
+    public void ejercicio2(int[] A) {
+        // Code
+    }
+
+    public void ejercicio3() {
+        // Code
+    }
+
+    public void ejercicio4() {
+        // Code
+    }
+}
